5a826fd29460a40fa8e7f4c665b7eca88fe5e580
arkoniko <104382075+arkoniko@users.noreply.github.com>
2023-12-17 22:12:51 +0100

    Add files via upload
    TYPESCRIPT

---
+++ dist/script.js
@@ -0,0 +1,20 @@
+var styleFolder = './dist/styles/';
+var styles = ['style-alternative.css', 'style-alternative2.css', 'style-alternative3.css'];
+styles.forEach(function (style) {
+    if (style.slice(-4) === ".css") {
+        var buttonContainer = document.getElementsByClassName("theme-buttons")[0];
+        var placeholder = document.createElement('div');
+        placeholder.innerHTML = "<button class=\"theme-button\">".concat(style.slice(0, -4), "</button>");
+        var button = placeholder.firstElementChild;
+        button.addEventListener("click", function (e) { switchTheme(styleFolder + style); });
+        buttonContainer.appendChild(button);
+    }
+});
+function switchTheme(theme) {
+    var links = document.getElementsByTagName('link');
+    for (var _i = 0, _a = links; _i < _a.length; _i++) {
+        var link = _a[_i];
+        if (link.rel === "stylesheet")
+            link.setAttribute("href", theme);
+    }
+}
